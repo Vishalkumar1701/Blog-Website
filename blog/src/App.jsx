f80297332087d9af2567778f81bc22bd262ecddd
Vishalkumar1701 <kvishalkumar.17.01.2000@gmail.com>
2024-06-17 12:41:59 +0530

    Created navbar section

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+
+import Header from './Component/Header'
+import Posts from './Pages/Posts'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-red-500'>hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/sign-in' element= {<Signin/>} />
+        <Route path='/sign-up' element= {<Signup/>} />
+        <Route path='/blog-post' element= {<Posts/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ blog/src/Component/Header.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import { Navbar, TextInput, Button } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaMoon} from 'react-icons/fa'
+
+
+const Header = () => {
+    const path = useLocation().pathname;
+
+  return (
+    <div>
+      <Navbar className='border-b-2'>
+        <Link to= "/" className='font-semibold self-center whitespace-nowrap text-sm dark:text-white'>
+            <span className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white px-2 py-1'>Writers</span>
+            <span className=''>Wanderlust</span>
+        </Link>
+        <form>
+            <TextInput 
+                type='text'
+                placeholder='Search...'
+                rightIcon={AiOutlineSearch}
+                className='hidden lg:inline'
+            />
+            
+        </form>
+        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+                <AiOutlineSearch/>
+            </Button>
+        <div className='flex gap-2 md:order-2'>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+                    <FaMoon />
+                </Button>
+                <Link to='/sign-in'>
+                    <Button className='' gradientDuoTone='purpleToBlue'>
+                        Sign In
+                    </Button>
+                </Link>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                    <Navbar.Link active={path === '/'} as={'div'}>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                    </Navbar.Link>
+                    <Navbar.Link active={path === '/blog-post'} as={'div'}>
+                        <Link to='/blog-post'>
+                            Posts
+                        </Link>
+                    </Navbar.Link>
+                </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
