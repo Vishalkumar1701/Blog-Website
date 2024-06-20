9a9d69ca021bf38e61790aceff86be726e3a670d
Vishalkumar1701 <kvishalkumar.17.01.2000@gmail.com>
2024-06-20 11:04:24 +0530

    create and run server

--- blog/src/Pages/Home.jsx
+++ blog/src/Pages/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import mainimg from '../assets/images/main.jpg'
+import MainCard from '../Component/MainCard'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='h-screen'
+      style={{
+        backgroundImage: `url(${mainimg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }}>
+
+      <div className='bg-black h-screen bg-opacity-35'>
+        <div className='pt-36 text-center font-bold text-white'>
+          <span className='border-8 lg:text-7xl md:text-4xl border-slate-100 px-7 py-4'>Writers Wanderlust</span>
+        </div>
+        <p className='text-center pt-36 text-white text-xl'>Exploring Through Words and Journeys</p>
+      </div>
+
+      <div>
+        <MainCard />
+      </div>
     </div>
   )
 }
